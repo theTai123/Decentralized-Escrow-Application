1359c913cfbc3cbd0186ed0dc2f81f2b0831e586
theTai123 <lamgiatai381@gmail.com>
2023-12-27 19:43:58 +0700

    Fix and update test.js

--- test/test.js
+++ test/test.js
@@ -9,10 +9,10 @@ describe('Escrow', function () {
   let arbiter2;
   const deposit = ethers.parseEther('1');
   beforeEach(async () => {
-    depositor = ethers.provider.getSigner(0);
-    beneficiary = ethers.provider.getSigner(1);
-    arbiter1 = ethers.provider.getSigner(2);
-    arbiter2 = ethers.provider.getSigner(3);
+    depositor = await ethers.provider.getSigner(0);
+    beneficiary = await ethers.provider.getSigner(1);
+    arbiter1 = await ethers.provider.getSigner(2);
+    arbiter2 = await ethers.provider.getSigner(3);
     const Escrow = await ethers.getContractFactory('Escrow');
     contract = await Escrow.deploy(
       arbiter1.getAddress(),
@@ -22,11 +22,11 @@ describe('Escrow', function () {
         value: deposit,
       }
     );
-    await contract.deployed();
+    await contract.waitForDeployment();
   });
 
   it('should be funded initially', async function () {
-    let balance = await ethers.provider.getBalance(contract.address);
+    let balance = await ethers.provider.getBalance(contract.target);
     expect(balance).to.eq(deposit);
   });
 
@@ -36,15 +36,22 @@ describe('Escrow', function () {
     });
   });
 
+  describe('after approval from the same arbiter', () => {
+    it('should revert', async () => {
+      await contract.connect(arbiter1).approve();
+      await expect(contract.connect(arbiter1).approve()).to.be.reverted;
+    });
+  });
+
   describe('after approval from the arbiter', () => {
     it('should transfer balance to beneficiary', async () => {
       const before = await ethers.provider.getBalance(beneficiary.getAddress());
       const approveTxn1 = await contract.connect(arbiter1).approve();
-      const approveTxn2 = await contract.connect(arbiter2).approve();
       await approveTxn1.wait();
+      const approveTxn2 = await contract.connect(arbiter2).approve();
       await approveTxn2.wait();
       const after = await ethers.provider.getBalance(beneficiary.getAddress());
-      expect(after.sub(before)).to.eq(deposit);
+      expect(after-before).to.eq(deposit);
     });
   });
 });
